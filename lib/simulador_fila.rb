fbabac0a46a0b6fc0acd520f5984b5579da02c35
Douglas Grund <douglasgrund@yahoo.com.br>
2024-06-10 16:02:28 -0300

    feat: adiciona método initialize à classe Cliente

--- lib/simulador_fila.rb
+++ lib/simulador_fila.rb
@@ -1,2 +1,8 @@
 class Cliente
   attr_accessor :chegada, :inicio_atendimento, :fim_atendimento
+
+  def initialize(chegada)
+    @chegada = chegada
+    @inicio_atendimento = nil
+    @fim_atendimento = nil
+  end
