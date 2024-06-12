b5f66f6bc0396f1b4c1a4d4887e4edd13d15c638
Douglas Grund <douglasgrund@yahoo.com.br>
2024-06-12 15:47:42 -0300

    feat: add classe SimuladorFila

--- lib/simulador_fila.rb
+++ lib/simulador_fila.rb
@@ -16,13 +16,26 @@ class Cliente
     @fim_atendimento - @chegada
   end
 
-  def simular(ciclos)
-    ciclos.times do
-      chegada_cliente
-      atender_cliente
+  class SimuladorFila
+    attr_accessor :fila, :tempo_atual, :clientes_atendidos, :total_tempo_espera, :total_tempo_sistema
+
+    def initialize
+      @fila = []
+      @tempo_atual = 0
+      @clientes_atendidos = 0
+      @total_tempo_espera = 0
+      @total_tempo_sistema = 0
+      @ocupacao_servidor = 0
     end
 
-    calcular_estatisticas
+    def simular(ciclos)
+      ciclos.times do
+        chegada_cliente
+        atender_cliente
+      end
+
+      calcular_estatisticas
+    end
   end
 
 end
